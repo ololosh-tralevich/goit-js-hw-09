2f24f85b8bb2105b858512214dd735afd588083c
ololosh-tralevich <karpeko76@gmail.com>
2022-01-20 13:43:31 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyDocument = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  bodyDocument.style.backgroundColor = randomColor;
+}
+
+buttonStart.addEventListener('click', event => {
+  timerId = setInterval(() => {
+    getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true; 
+  buttonStop.disabled = false; 
+});
+
+if (!buttonStart.disabled) {
+    buttonStop.disabled = true;
+}
+
+buttonStop.addEventListener('click', event => {
+    clearInterval(timerId);
+  buttonStart.disabled = false; 
+  buttonStop.disabled = true; 
+});
