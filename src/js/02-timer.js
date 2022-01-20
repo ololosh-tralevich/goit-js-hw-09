f0da895791e59a1250f2480d2a0de3bf35efbcf2
ololosh-tralevich <karpeko76@gmail.com>
2022-01-20 17:25:36 +0200

    done second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,8 +2,14 @@ import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const body = document.querySelector('body');
+
 const inputData = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
+const counterDays = document.querySelector('.value[data-days]');
+const counterHours = document.querySelector('.value[data-hours]');
+const counterMinutes = document.querySelector('.value[data-minutes]');
+const counterSeconds = document.querySelector('.value[data-seconds]');
 
 startButton.disabled = true;
 const options = {
@@ -19,27 +25,40 @@ const options = {
       return;
     }
     startButton.disabled = false;
+
+    startButton.addEventListener('click', event => {
+    startButton.disabled = true;
+    inputData.disabled = true;
+      let timerId = setInterval(() => {
+        let remainingMs = selectedDates[0] - new Date();
+        if (remainingMs <= 1000) {
+          clearInterval(timerId);
+          body.style.backgroundColor = 'red';
+        }
+        convertMs(remainingMs);
+      }, 1000);
+      function convertMs(remainingMs) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        // Remaining days
+        const days = Math.floor(remainingMs / day);
+        // Remaining hours
+        const hours = Math.floor((remainingMs % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((remainingMs % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((remainingMs % day) % hour) % minute) / second);
+
+        counterDays.textContent = String(days).padStart(2, 0);
+        counterHours.textContent = String(hours).padStart(2, 0);
+        counterMinutes.textContent = String(minutes).padStart(2, 0);
+        counterSeconds.textContent = String(seconds).padStart(2, 0);
+      }
+    });
   },
 };
 flatpickr(inputData, options);
-
-startButton.addEventListener('click', event => {
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-  }
-});
